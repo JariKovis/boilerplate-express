deead2daafcb6e3c99b8e173be41e412c76bd9fb
Jari Kovalainen <itse@jarikovalainen.com>
2024-01-02 17:04:50 +0000

    Update 3 files

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 let express = require("express");
 let app = express();
+const absolutePath = __dirname + "/views/";
 
-console.log("Hello World");
+//console.log("Hello World");
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
