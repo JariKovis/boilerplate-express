fe3b4d0711e52d45ff965c368bc9ed818630102e
Jari Kovalainen <itse@jarikovalainen.com>
2024-01-03 13:00:16 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require("express");
 let app = express();
 const absolutePath = __dirname + "/views/";
 
+app.use("/public", express.static(__dirname + "/public"));
 //console.log("Hello World");
 
 app.get("/", function (req, res) {
